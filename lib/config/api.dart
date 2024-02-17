245086ee0fa5c13b55b4476bd77a689c9bd38be5
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-16 21:16:55 -0300

    Recebendo a lista de videos.

---
+++ lib/config/api.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:fav_tube/config/api_key.dart';
+import 'package:fav_tube/models/video.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  Future<void> search(String searsh) async {
+    Uri searshUri = Uri(
+        scheme: 'https',
+        host: 'www.googleapis.com',
+        path: '/youtube/v3/search',
+        queryParameters: {
+          'part': 'snippet',
+          'q': searsh,
+          'type': 'video',
+          'key': API_KEY,
+          'maxResults': '10'
+        });
+    http.Response response = await http.get(searshUri);
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+    if (response.statusCode == 200) {
+      var decoded = jsonDecode(response.body);
+
+      List<Video> videos = decoded['items'] != null ? decoded['items'].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList() : [];
+      return videos;
+    } else {
+      throw Exception('Falha ao carregar os videos.');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:fav_tube/pages/page_home.dart';
+import 'package:fav_tube/config/api.dart';
+import 'package:fav_tube/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Api api = Api();
+  api.search('f1');
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
