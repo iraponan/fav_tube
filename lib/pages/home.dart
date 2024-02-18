7589f85e37b81dc1dffb08f5ff0a34aed816778c
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-17 21:37:36 -0300

    Criando o layout da tela principal.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 40,
+          child: Image.asset('assets/images/youtube.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              '0',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.star),
+            style: IconButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+            style: IconButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
